6e3aef07a41a2caae95c3a32937797d265d40b35
Mehedi Hasan Bappi <mehedibubt34@gmail.com>
2022-09-17 22:06:38 +0600

    initial commit

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,8 +1,3 @@
-// require("@openzeppelin/test-helpers/configure")({
-//   provider: web3.currentProvider,
-//   environment: "truffle",
-// });
-
 const QueryContract = artifacts.require("QueryContract");
 
 module.exports = async function (deployer) {
